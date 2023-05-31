f5f923aff2ddf2f7784cbce93d0aa326bdc19b3b
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-05-31 14:16:46 -0700

    types of buttons and valtate it

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -3,7 +3,7 @@ import { TrashIcon } from '@heroicons/vue/24/outline';
 import BaseButton from './BaseButton.vue';
 import BaseSelect from './BaseSelect.vue';
 import { ref } from 'vue';
-import { PERIOD_SELECT_OPTIONS } from '../constants';
+import { PERIOD_SELECT_OPTIONS, BUTTON_TYPE_DANGER } from '../constants';
 import { isActivityValid} from '../validators'
 
 defineProps({
@@ -22,7 +22,7 @@ const secondsToComplete = ref(null)
   <li
     class="flex flex-col gap-2 p-4">
     <div class="flex items-center gap-2">
-      <BaseButton >
+      <BaseButton :type="BUTTON_TYPE_DANGER">
         <TrashIcon class="h-8"/>
       </BaseButton>
       <span class="truncate text-xl">{{ activity }}</span>
